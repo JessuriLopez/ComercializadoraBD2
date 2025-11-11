9d996683a630f0ce0c215dd29aacd48a9e2a9575
jessu <jessuri.lopez@unah.hn>
2025-11-11 16:56:25 -0600

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace ComercializadoraPM_App
+{
+    public class Conexion
+    {
+        private SqlConnection conexion = new SqlConnection("Server=localhost;Database=ComercializadoraPM;Trusted_Connection=True;");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+    }
+}
+
